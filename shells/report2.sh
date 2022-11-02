f85d56e1eba187c087e105185e3669dc31e33f6e
AltaFrue <fkrgns56@gmail.com>
2022-11-02 12:09:52 +0900

    Shell Script3

---
+++ shells/report2.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]; then
+  echo "Enter the parameter!!"
+elif [ $# -ne 3 ]; then
+  echo "This program is have just three parameters."
+else
+  if [ $2 == '+' ]; then
+    echo "$1 + $3 = `expr $1 \+ $3`"
+  elif [ $2 == '-' ]; then
+    echo "$1 - $3 = `expr $1 \- $3`"
+  elif [ $2 == "x" ]; then
+    echo "$1 x $3 = `expr $1 \* $3`"
+  elif [ $2 == '/' ]; then
+    echo "$1 / $3 = `expr $1 \/ $3`"
+  else
+    echo "second parameter is only '+ - x /'"
+  fi
+fi
